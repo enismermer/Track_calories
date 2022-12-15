f6817063917c4aa254dd8201b556524629f73348
Enis <enis.mermer.simplon@gmail.com>
2022-12-15 17:49:13 +0100

    modified

--- index.php
+++ index.php
@@ -1,26 +1,62 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link href="assets/css/app.css" rel="stylesheet">
-    <title>Calories food</title>
-</head>
-<body>
-    <div class="container text-center">
-        <div class="row">
-            <div class="col-sm-12 col-md-4 col-lg-6">
-            Column
+<?php
+
+// Connexion à la BDD
+
+// Un user s'est connecté
+
+// On stock les informations du user dans une variable
+
+$user = [
+    "id" => 1,
+    "name" => "Enis",
+    "age"=> 22,
+    "sexe" => "homme",
+    "weight" => 75,
+    "size" => 185,
+    "IMC" => 23.4,
+    "email" => "blabla@gmail.com",
+    "isLogged" => true
+];
+
+if (!$user["isLogged"]) {
+    header("location:login.php");
+    exit;
+}
+
+$page = [
+    "title" => "Track Calorie - Accueil"
+];
+
+include_once('includes/header.php');
+
+?>
+
+    <div class="container">
+        <header>
+            <div class="title">Track Calories</div>
+            <div class="profile"><?php echo $user['name']; ?></div>
+        </header>
+
+        <section class="dataUser">
+            <div>Graph</div>
+            <div>IMC</div>
+            <div><?php echo $user['weight']; ?>Kg</div>
+        </section>
+
+        <section class="date">
+            <div><?php echo date('l d M Y'); ?></div>
+        </section>
+            
+        <section class="list">
+            <div class="food">
+                <div class="titleFood">Big Mac</div>
+                <div class="kgFood">504kg</div>
             </div>
-            <div class="col-sm-12 col-md-4 col-lg-6">
-            Column
-            </div>
-            <div class="col-sm-12 col-md-4 col-lg-12">
-            Column
-            </div>
-        </div>
+        </section>
+
+        <footer>
+                <button>+</button>
+            </nav>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-</body>
-</html>
+
+<?php include_once('includes/footer.php'); ?>
---
+++ login.php
@@ -0,0 +1,17 @@
+<?php
+
+    $page = [
+        "title" => "Track Calorie - Accueil"
+    ];
+
+    
+    include_once('includes/header.php');
+?>
+
+<h1>Login Page</h1>
+
+<?php
+
+    include_once('includes/footer.php');
+
+?>
