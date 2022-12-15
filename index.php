ec666168c1ccfc784ee148b427dc9b493b25b35e
Enis <enis.mermer.simplon@gmail.com>
2022-12-15 15:32:12 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="assets/css/app.css" rel="stylesheet">
+    <title>Calories food</title>
+</head>
+<body>
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-sm-12 col-md-4 col-lg-6">
+            Column
+            </div>
+            <div class="col-sm-12 col-md-4 col-lg-6">
+            Column
+            </div>
+            <div class="col-sm-12 col-md-4 col-lg-12">
+            Column
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
